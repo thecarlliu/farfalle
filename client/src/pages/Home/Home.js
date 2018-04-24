8acf60e59ce7e7067fe8f21ab39d87709d9272ed
CarL Liu <thecarlliu99@CarLs-MacBook-Air.local>
2018-04-24 13:41:39 -0500

    Skeleton is ready

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Home.css";
+import Endorsements from "../../components/Endorsements";
+import ImageGrid1 from "../../components/ImageGrid1";
+import ImageGrid2 from "../../components/ImageGrid2";
+import ImageGrid3 from "../../components/ImageGrid3";
+
+
+const images = [
+    {
+        name: "",
+        src: ""
+    }
+];
+
+const Home = () => (
+    <div className="grid-x">
+
+    </div>
+);
+
+export default Home;
